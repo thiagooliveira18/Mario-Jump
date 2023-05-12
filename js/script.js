1287cb0c4b3168f1724e618bafe2ab3256f1f919
Thiago Oliveira <thiago.oliveira.ds18@gmail.com>
2023-05-12 12:26:05 -0300

    Score board

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
-
+const score = document.querySelector('.score');
+let count=0;
 const jump = () => {
     mario.classList.add('jump');
 
@@ -12,19 +13,30 @@ const jump = () => {
 const loop = setInterval(()=>{
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    
+    
+    count += 0.1;
+    console.log(count);
+    score.innerHTML = count.toFixed(0);
 
-    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 70) {
+
+    if(gameOver(pipePosition, marioPosition)) {
+       clearInterval(loop);
+    }
+},10);
+
+const gameOver = (posPipe, posMario) => {
+    if(posPipe <= 120 && posPipe > 0 && posMario < 70) {
         pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
+        pipe.style.left = `${posPipe}px`;
         mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
+        mario.style.bottom = `${posMario}px`;
 
         mario.src = './images/game-over.png';
         mario.style.width = '60px';
         mario.style.marginLeft = '50px';
-
-        clearInterval(loop);
-    }
-},10);
+        return true;
+    }    
+}
 
 document.addEventListener('keydown', jump);
